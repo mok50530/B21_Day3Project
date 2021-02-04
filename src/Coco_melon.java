e48a6344cfbf2bea0d9f9844cf05a174dbf0fe68
mok50531 <mok50531@gmail.com>
2021-02-03 18:23:04 -0600

    final work is done

---
+++ src/Coco_melon.java
@@ -0,0 +1,8 @@
+public class Coco_melon {
+
+    // I do not watch coco-melon
+    // But I heard about it from my friend
+    // I still do not watch coco-melon
+
+
+}
--- src/Cocomelon.java
+++
@@ -1,7 +0,0 @@
-public class Cocomelon {
-
-    // I do not watch cocomelon
-    // But I heard about it from my friend
-
-
-}
